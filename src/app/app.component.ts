694308a75a6e38528f265fc979e12f2bccd0d85f
kakkarot9712 <vikalpgandha9712@gmail.com>
2022-11-11 19:42:02 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, CommonModule, HeaderComponent, FooterComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portfolio';
+}
