a1b9ac6935c6bc1e3a74d8e268979b4c832fb73a
kakkarot9712 <vikalpgandha9712@gmail.com>
2022-11-14 17:49:56 +0530

    add about webpage about web page is responsive now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 const routes: Routes = [
   {path: '', pathMatch:'full', redirectTo:'home'},
   {path: 'home', component: HomeComponent},
-  {path: 'contacts', component: ContactsComponent}
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'resume', component: ResumeComponent},
 ];
 
 @NgModule({
